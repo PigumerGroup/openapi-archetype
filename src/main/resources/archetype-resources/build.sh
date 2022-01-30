83facfac6aab2b34d095a8c6b1974df2e74158dd
Shigeki Shoji <takesection@gmail.com>
2022-01-30 23:14:25 +0900

    v0.1.17

--- src/main/resources/archetype-resources/build.sh
+++ src/main/resources/archetype-resources/build.sh
@@ -1,6 +1,6 @@
 rm -rf out
 
-curl -L https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.1.1/openapi-generator-cli-5.1.1.jar -o openapi-generator-cli.jar
+curl -L https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.3.1/openapi-generator-cli-5.3.1.jar -o openapi-generator-cli.jar
 
 java -jar openapi-generator-cli.jar batch services/service-spring-appservice.yaml
 java -Xmx1024M -DloggerPath=conf/log4j.properties -DsupportingFiles -jar openapi-generator-cli.jar batch services/service-spring-controller.yaml
